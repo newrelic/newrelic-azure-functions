0294a5a2624bcffea7e1b01f760e019aed4901f8
Matilda Smeds <msmeds@newrelic.com>
2020-07-07 14:38:22 +0200

    [LOGGING-2708] Add prettier lint

--- BlobForwarder/index.js
+++ BlobForwarder/index.js
@@ -1,217 +1,236 @@
 /**
-* Azure function to read from Blob Storage and forward logs to New Relic.
-*/
+ * Azure function to read from Blob Storage and forward logs to New Relic.
+ */
 
-"use strict"
+'use strict';
 
-var https = require('https')
-var url = require('url')
-var zlib = require('zlib')
+var https = require('https');
+var url = require('url');
+var zlib = require('zlib');
 
-const VERSION = '0.0.0-development'
+const VERSION = '0.0.0-development';
 
 // Global constants
-const NR_LICENSE_KEY = process.env.NR_LICENSE_KEY
-const NR_INSERT_KEY = process.env.NR_INSERT_KEY
-const NR_ENDPOINT = process.env.NR_ENDPOINT || 'https://log-api.newrelic.com/log/v1'
-const NR_TAGS = process.env.NR_TAGS // Semicolon-seperated tags
-const NR_LOGS_SOURCE = 'azure'
-const NR_MAX_PAYLOAD_SIZE = 1000 * 1024
-const NR_MAX_RETRIES = process.env.NR_MAX_RETRIES || 3
-const NR_RETRY_INTERVAL =  process.env.NR_RETRY_INTERVAL || 2000 // default: 2 seconds
+const NR_LICENSE_KEY = process.env.NR_LICENSE_KEY;
+const NR_INSERT_KEY = process.env.NR_INSERT_KEY;
+const NR_ENDPOINT =
+  process.env.NR_ENDPOINT || 'https://log-api.newrelic.com/log/v1';
+const NR_TAGS = process.env.NR_TAGS; // Semicolon-seperated tags
+const NR_LOGS_SOURCE = 'azure';
+const NR_MAX_PAYLOAD_SIZE = 1000 * 1024;
+const NR_MAX_RETRIES = process.env.NR_MAX_RETRIES || 3;
+const NR_RETRY_INTERVAL = process.env.NR_RETRY_INTERVAL || 2000; // default: 2 seconds
 
 module.exports = async function main(context, myBlob) {
   if (!NR_LICENSE_KEY && !NR_INSERT_KEY) {
-    context.log.error('You have to configure either your LICENSE key or insights insert key. '
-      + 'Please follow the instructions in README')
-    return
+    context.log.error(
+      'You have to configure either your LICENSE key or insights insert key. ' +
+        'Please follow the instructions in README'
+    );
+    return;
   }
-  let logs
+  let logs;
   if (typeof myBlob === 'string') {
-    logs = myBlob.trim().split('\n')
+    logs = myBlob.trim().split('\n');
   } else if (Buffer.isBuffer(myBlob)) {
-    logs = myBlob.toString('utf8').trim().split('\n')
+    logs = myBlob.toString('utf8').trim().split('\n');
   } else {
-    logs = JSON.stringify(myBlob).trim().split('\n')
+    logs = JSON.stringify(myBlob).trim().split('\n');
   }
-  let buffer = transformData(logs, context)
+  let buffer = transformData(logs, context);
   if (buffer.length === 0) {
-    context.log.warn('logs format is invalid')
-    return
+    context.log.warn('logs format is invalid');
+    return;
   }
-  let compressedPayload
-  let payloads = generatePayloads(buffer, context)
+  let compressedPayload;
+  let payloads = generatePayloads(buffer, context);
   for (const payload of payloads) {
     try {
-      compressedPayload = await compressData(JSON.stringify(payload))
+      compressedPayload = await compressData(JSON.stringify(payload));
       try {
-        await retryMax(httpSend, NR_MAX_RETRIES, NR_RETRY_INTERVAL, [compressedPayload, context])
-        context.log("Logs payload successfully sent to New Relic")
+        await retryMax(httpSend, NR_MAX_RETRIES, NR_RETRY_INTERVAL, [
+          compressedPayload,
+          context,
+        ]);
+        context.log('Logs payload successfully sent to New Relic');
       } catch {
-        context.log.error("Max retries reached: failed to send logs payload to New Relic")
+        context.log.error(
+          'Max retries reached: failed to send logs payload to New Relic'
+        );
       }
     } catch {
-      context.log.error("Error during payload  compression")
+      context.log.error('Error during payload  compression');
     }
   }
-}
+};
 
 function compressData(data) {
-  return new Promise( (resolve,reject) => {
+  return new Promise((resolve, reject) => {
     zlib.gzip(data, (e, compressedData) => {
       if (!e) {
-        resolve(compressedData)
+        resolve(compressedData);
       } else {
-        reject({'error':e,'res':null})
+        reject({ error: e, res: null });
       }
-    })
-  })
+    });
+  });
 }
 
 function generatePayloads(logs, context) {
   const common = {
-    'attributes': {
-      'plugin': {
-        'type': NR_LOGS_SOURCE,
-        'version': VERSION
+    attributes: {
+      plugin: {
+        type: NR_LOGS_SOURCE,
+        version: VERSION,
       },
-      'azure': {
-        'forwardername': context.executionContext.functionName,
-        'invocationid': context.executionContext.invocationId
+      azure: {
+        forwardername: context.executionContext.functionName,
+        invocationid: context.executionContext.invocationId,
       },
-      'tags': getTags(),
-    }
-  }
-  let payload = [{
-    'common': common,
-    'logs': []
-  }]
-  let payloads = []
+      tags: getTags(),
+    },
+  };
+  let payload = [
+    {
+      common: common,
+      logs: [],
+    },
+  ];
+  let payloads = [];
 
-  logs.forEach(logLine => {
-    const log = addMetadata(logLine)
-    if ((JSON.stringify(payload).length + JSON.stringify(log).length) < NR_MAX_PAYLOAD_SIZE) {
-      payload[0].logs.push(log)
+  logs.forEach((logLine) => {
+    const log = addMetadata(logLine);
+    if (
+      JSON.stringify(payload).length + JSON.stringify(log).length <
+      NR_MAX_PAYLOAD_SIZE
+    ) {
+      payload[0].logs.push(log);
     } else {
-      payloads.push(payload)
-      payload = [{
-        'common': common,
-        'logs': []
-      }]
-      payload[0].logs.push(log)
+      payloads.push(payload);
+      payload = [
+        {
+          common: common,
+          logs: [],
+        },
+      ];
+      payload[0].logs.push(log);
     }
-  })
-  payloads.push(payload)
-  return payloads
-};
+  });
+  payloads.push(payload);
+  return payloads;
+}
 
 function getTags() {
-  const tagsObj = {}
+  const tagsObj = {};
   if (NR_TAGS) {
-    const tags = NR_TAGS.split(';')
-    tags.forEach(tag => {
-      const keyValue = tag.split(':')
+    const tags = NR_TAGS.split(';');
+    tags.forEach((tag) => {
+      const keyValue = tag.split(':');
       if (keyValue.length > 1) {
-        tagsObj[keyValue[0]] = keyValue[1]
+        tagsObj[keyValue[0]] = keyValue[1];
       }
-    })
-  };
-  return tagsObj
-};
+    });
+  }
+  return tagsObj;
+}
 
 function addMetadata(logEntry) {
   if (
     logEntry.resourceId !== undefined &&
-        typeof logEntry.resourceId === 'string' &&
-        logEntry.resourceId.toLowerCase().startsWith('/subscriptions/')
+    typeof logEntry.resourceId === 'string' &&
+    logEntry.resourceId.toLowerCase().startsWith('/subscriptions/')
   ) {
-    let resourceId = logEntry.resourceId.toLowerCase().split('/')
+    let resourceId = logEntry.resourceId.toLowerCase().split('/');
     if (resourceId.length > 2) {
-      logEntry.metadata = {}
-      logEntry.metadata.subscriptionId = resourceId[2]
+      logEntry.metadata = {};
+      logEntry.metadata.subscriptionId = resourceId[2];
     }
     if (resourceId.length > 4) {
-      logEntry.metadata.resourceGroup = resourceId[4]
+      logEntry.metadata.resourceGroup = resourceId[4];
     }
     if (resourceId.length > 6 && resourceId[6]) {
-      logEntry.metadata.source = resourceId[6].replace('microsoft.', 'azure.')
+      logEntry.metadata.source = resourceId[6].replace('microsoft.', 'azure.');
     }
   }
-  return logEntry
-};
+  return logEntry;
+}
 
 function transformData(logs, context) {
   // buffer is an array of JSON objects
-  let buffer = []
+  let buffer = [];
 
-  let parsedLogs = parseData(logs, context)
+  let parsedLogs = parseData(logs, context);
 
   // type JSON object
-  if (!Array.isArray(parsedLogs)
-    && typeof parsedLogs === 'object'
-    && parsedLogs !== null) {
+  if (
+    !Array.isArray(parsedLogs) &&
+    typeof parsedLogs === 'object' &&
+    parsedLogs !== null
+  ) {
     if (parsedLogs.records !== undefined) {
-      context.log("Type of logs: records Object")
-      parsedLogs.records.forEach(log => buffer.push(log))
-      return buffer
+      context.log('Type of logs: records Object');
+      parsedLogs.records.forEach((log) => buffer.push(log));
+      return buffer;
     }
-    context.log("Type of logs: JSON Object")
-    buffer.push(parsedLogs)
-    return buffer
+    context.log('Type of logs: JSON Object');
+    buffer.push(parsedLogs);
+    return buffer;
   }
 
   // Bad Format
   if (!Array.isArray(parsedLogs)) {
-    return buffer
+    return buffer;
   }
 
   if (typeof parsedLogs[0] === 'object') {
     // type JSON records
     if (parsedLogs[0].records !== undefined) {
-      context.log("Type of logs: records Array")
-      parsedLogs.forEach(message => {
-        message.records.forEach(log => buffer.push(log))
-      })
-      return buffer
-    }  // type JSON array
-    context.log("Type of logs: JSON Array")
+      context.log('Type of logs: records Array');
+      parsedLogs.forEach((message) => {
+        message.records.forEach((log) => buffer.push(log));
+      });
+      return buffer;
+    } // type JSON array
+    context.log('Type of logs: JSON Array');
     // normally should be "buffer.push(log)" but that will fail if the array mixes JSON and strings
-    parsedLogs.forEach(log => buffer.push({ message: log }))
+    parsedLogs.forEach((log) => buffer.push({ message: log }));
     // Our API can parse the data in "log" to a JSON and ignore "message", so we are good!
-    return buffer
+    return buffer;
   }
-  if (typeof parsedLogs[0] === 'string') { // type string array
-    context.log("Type of logs: string Array")
-    parsedLogs.forEach(logString => buffer.push({ message: logString }))
-    return buffer
+  if (typeof parsedLogs[0] === 'string') {
+    // type string array
+    context.log('Type of logs: string Array');
+    parsedLogs.forEach((logString) => buffer.push({ message: logString }));
+    return buffer;
   }
-  return buffer
-};
+  return buffer;
+}
 
 function parseData(logs, context) {
-  let newLogs = logs
+  let newLogs = logs;
 
   if (!Array.isArray(logs)) {
     try {
-      newLogs = JSON.parse(logs) // for strings let's see if we can parse it into Object
+      newLogs = JSON.parse(logs); // for strings let's see if we can parse it into Object
     } catch {
-      context.log.warn("cannot parse logs to JSON")
+      context.log.warn('cannot parse logs to JSON');
     }
   } else {
-    newLogs = logs.map(log => {     // for arrays let's see if we can parse it into array of Objects
+    newLogs = logs.map((log) => {
+      // for arrays let's see if we can parse it into array of Objects
       try {
-        return JSON.parse(log)
+        return JSON.parse(log);
       } catch {
-        return log
+        return log;
       }
-    })
+    });
   }
-  return newLogs
-};
+  return newLogs;
+}
 
 function httpSend(data, context) {
-  return new Promise( (resolve,reject) => {
-    const urlObj = url.parse(NR_ENDPOINT)
+  return new Promise((resolve, reject) => {
+    const urlObj = url.parse(NR_ENDPOINT);
     const options = {
       hostname: urlObj.hostname,
       port: 443,
@@ -220,39 +239,39 @@ function httpSend(data, context) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Content-Encoding': 'gzip'
-      }
-    }
+        'Content-Encoding': 'gzip',
+      },
+    };
 
     if (NR_LICENSE_KEY) {
-      options.headers["X-License-Key"] = NR_LICENSE_KEY
+      options.headers['X-License-Key'] = NR_LICENSE_KEY;
     } else {
-      options.headers["X-Insert-Key"] = NR_INSERT_KEY
+      options.headers['X-Insert-Key'] = NR_INSERT_KEY;
     }
 
     var req = https.request(options, (res) => {
-      var body = ''
-      res.setEncoding('utf8')
+      var body = '';
+      res.setEncoding('utf8');
       res.on('data', (chunk) => {
-        body += chunk // don't really do anything with body
-      })
+        body += chunk; // don't really do anything with body
+      });
       res.on('end', () => {
-        context.log('Got response:' + res.statusCode)
+        context.log('Got response:' + res.statusCode);
         if (res.statusCode === 202) {
-          resolve(body)
+          resolve(body);
         } else {
-          reject({'error':null,'res':res})
+          reject({ error: null, res: res });
         }
-      })
-    })
+      });
+    });
 
     req.on('error', (e) => {
-      reject({'error':e,'res':null})
-    })
-    req.write(data)
-    req.end()
-  })
-};
+      reject({ error: e, res: null });
+    });
+    req.write(data);
+    req.end();
+  });
+}
 
 /**
  * Retry with Promise
@@ -263,16 +282,16 @@ function httpSend(data, context) {
  * @returns A promise that resolves to the final result
  */
 
-function retryMax(fn,retry,interval,fnParams) {
-  return fn.apply(this,fnParams).catch( err => {
+function retryMax(fn, retry, interval, fnParams) {
+  return fn.apply(this, fnParams).catch((err) => {
     return retry > 1
-      ? wait(interval).then(()=> retryMax(fn,retry - 1,interval, fnParams))
-      : Promise.reject(err)
-  })
-};
+      ? wait(interval).then(() => retryMax(fn, retry - 1, interval, fnParams))
+      : Promise.reject(err);
+  });
+}
 
 function wait(delay) {
-  return new Promise((fulfill)=> {
-    setTimeout(fulfill, delay || 0)
-  })
-};
+  return new Promise((fulfill) => {
+    setTimeout(fulfill, delay || 0);
+  });
+}
