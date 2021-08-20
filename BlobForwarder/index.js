3b66bebb30825d2eaaf62371b61b07bb3ba186c6
jlenis <jlenis@newrelic.com>
2021-08-20 12:51:38 +0200

    Fix Eslint issues

--- BlobForwarder/index.js
+++ BlobForwarder/index.js
@@ -53,15 +53,15 @@ module.exports = async function main(context, myBlob) {
           context,
         ]);
         context.log('Logs payload successfully sent to New Relic.');
-      } catch(e) {
+      } catch (e) {
         context.log.error(
           'Max retries reached: failed to send logs payload to New Relic'
         );
-        context.log.error('Exception:', JSON.stringify(e));
+        context.log.error('Exception: ', JSON.stringify(e));
       }
-    } catch(e) {
+    } catch (e) {
       context.log.error('Error during payload compression.');
-      context.log.error('Exception:', JSON.stringify(e));
+      context.log.error('Exception: ', JSON.stringify(e));
     }
   }
 };
